b9fb75624639d1bd294367b6327225688aeb3938
Julian Krustev <juliankrustev2018@gmail.com>
2022-10-31 11:35:34 +0200

    Resolved tasks for useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random Title");
+
+  const handleClick = () => {
+    text === "Random Title" ? setText("Hello World") : setText("Random Title");
+  };
+
+  return (
+    <>
+      <h1>{text}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Julian",
+    age: 38,
+    message: "Random Message",
+  });
+
+  const handleClick = () => {
+    const newMessage = " Hello World";
+
+    setPerson({ ...person, message: newMessage });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>{person.message}</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
