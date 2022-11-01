e7bdf4dd2a188f017733fd8d912626ae6820dd71
Julian Krustev <juliankrustev2018@gmail.com>
2022-11-01 10:27:13 +0200

    Resolved useEffect taskas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-import Final from "./tutorial/1-useState/final/5-useState-counter";
+import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      <Final />
     </div>
   );
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    value > 0
+      ? (document.title = `New Messages(${value})`)
+      : (document.title = document.title);
+  });
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" type="button" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
