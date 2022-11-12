cdfdeaa92ce80ba2689eccf7a962ee6c182bf048
Julian Krustev <juliankrustev2018@gmail.com>
2022-11-12 09:46:11 +0200

    Prop-types task was completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
-import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
+import Setup from "./tutorial/10-prop-types/setup";
+import Final from "./tutorial/10-prop-types//final";
 
 function App() {
   return (
