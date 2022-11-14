71ab3a50ad90599f45bb1412e744c5604ec82263
Julian Krustev <juliankrustev2018@gmail.com>
2022-11-14 13:54:28 +0200

    Update react-router-dom to version 6.4.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/10-prop-types/setup";
-import Final from "./tutorial/10-prop-types//final";
+import Setup from "./tutorial/11-react-router/setup/";
 
 function App() {
   return (
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Person from "./Person";
 // navbar
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/people" element={<People />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
